1fea631b8a80d65777afac80fd06cbe5173a0222
MaxHord <max981hord@gmail.com>
2021-03-10 23:58:46 +0200

    Updates

---
+++ precache-manifest.a0da4b36386c309899f1920497127281.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d736f42ab16e288e232f97f859cb6338",
+    "url": "/front-end/index.html"
+  },
+  {
+    "revision": "bd7c0e5458244bf4ae32",
+    "url": "/front-end/static/css/main.997ca0f9.chunk.css"
+  },
+  {
+    "revision": "cee47f48581104bb96cf",
+    "url": "/front-end/static/js/2.3b2006f1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/front-end/static/js/2.3b2006f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7c0e5458244bf4ae32",
+    "url": "/front-end/static/js/main.328220cc.chunk.js"
+  },
+  {
+    "revision": "c5e2c4b6c18dbe30303c",
+    "url": "/front-end/static/js/runtime-main.11274380.js"
+  },
+  {
+    "revision": "722c5f898bbca8b2eb3fce0287688326",
+    "url": "/front-end/static/media/Gotham-Bold.722c5f89.otf"
+  },
+  {
+    "revision": "6f8eacd611b2414da7d46d7fba0b02e8",
+    "url": "/front-end/static/media/Gotham-Book.6f8eacd6.otf"
+  },
+  {
+    "revision": "39b5ff14a54114e9ae92136578a0e2d3",
+    "url": "/front-end/static/media/Gotham-Medium.39b5ff14.otf"
+  },
+  {
+    "revision": "d75d7f70aa160104a45a743eae7a8a7e",
+    "url": "/front-end/static/media/Myriad-Pro.d75d7f70.otf"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/front-end/static/media/Star_Wars_logo.5d100186.png"
+  },
+  {
+    "revision": "d7a4759544e2c479c60bc1b34f802548",
+    "url": "/front-end/static/media/baby-yoda.d7a47595.jpg"
+  },
+  {
+    "revision": "0110e797d8bce60e83587be35dcddf62",
+    "url": "/front-end/static/media/cart1.0110e797.jpg"
+  },
+  {
+    "revision": "63e907e1cb5e11aa9fa578dfb39f25c6",
+    "url": "/front-end/static/media/cart4.63e907e1.jpg"
+  },
+  {
+    "revision": "49a29b89e6a2381468c33166667839a5",
+    "url": "/front-end/static/media/karyna.49a29b89.jpg"
+  },
+  {
+    "revision": "48ea1ffa7b62818b68ea06e849d3e307",
+    "url": "/front-end/static/media/max.48ea1ffa.jpg"
+  },
+  {
+    "revision": "a0c29d9e5c1268c5a471d312a38b7f38",
+    "url": "/front-end/static/media/stars-bg.a0c29d9e.jpg"
+  }
+]);
